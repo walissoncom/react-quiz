1bd13f9061ec6228364c8531d657f9048e8a11e0
Walisson Rodrigues <contact@walisson.com>
2020-07-22 14:31:07 +1000

    Implemented checkAnswer & nextQuestion functions

--- react-quiz/src/App.tsx
+++ react-quiz/src/App.tsx
@@ -44,11 +44,39 @@ const App = () => {
   }
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (!gameOver) {
 
+      // Users answer
+      const answer = e.currentTarget.value;
+
+      // Check answer againt correct answer
+      const correct = questions[number].correct_answer === answer;
+
+      // Add score if answer is correct
+      if (correct) setScore(prev => prev + 1);
+
+      // Save answer in the array for user answers
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      };
+
+      setUserAnswers(prev => [...prev, answerObject]);
+    }
   }
 
   const nextQuestion = () => {
 
+    // Move on to the next question if not the last question
+    const nextQuestion = number + 1;
+
+    if(nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true);
+    } else {
+      setNumber(nextQuestion);
+    }
   }
   
   return (
