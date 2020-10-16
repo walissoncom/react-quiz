6164f08c550d43ca0bec7c390bd227e3ec440f94
Walisson Rodrigues <contact@walisson.com>
2020-10-17 02:48:23 +1100

    Style adjustments

--- react-quiz/src/components/QuestionCard.styles.ts
+++ react-quiz/src/components/QuestionCard.styles.ts
@@ -1,28 +1,28 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    width: 600px;
-    background: #FFF;
-    border-radius: 3px;
-    margin: 25px;
-    padding: 20px 50px 40px;
-    box-shadow: 0px 5px 10px rgba(0, 0, 0, .25);
-    text-align: center;
+  width: 600px;
+  background: #fff;
+  border-radius: 3px;
+  margin: 25px;
+  padding: 20px 50px 40px;
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+  text-align: center;
 
-    p {
-        font-size 1rem;
-        margin: 20px 0 25px;
+  p {
+    font-size: 1rem;
+    margin: 20px 0 25px;
 
-         &.number {
-             margin-bottom: 33px;
-         }
+    &.number {
+      margin-bottom: 33px;
     }
+  }
 `;
 
 type ButtonWrapperProps = {
-    correct: boolean;
-    userClicked: boolean;
-}
+  correct: boolean;
+  userClicked: boolean;
+};
 
 export const ButtonWrapper = styled.div<ButtonWrapperProps>`
     transition: all .3s ease;
@@ -40,12 +40,11 @@ export const ButtonWrapper = styled.div<ButtonWrapperProps>`
         padding: 10px;
         color: #444;
         background: ${({ correct, userClicked }) =>
-            correct
-                ? 'linear-gradient(90deg, #28A745, #05B304); color: #FFF;'
-                : !correct && userClicked
-                ? 'linear-gradient(90deg, #DC3545, #CE0206); color: #FFF;'
-                : '#EFEFEF'
-            };
+          correct
+            ? "linear-gradient(90deg, #28A745, #05B304); color: #FFF;"
+            : !correct && userClicked
+            ? "linear-gradient(90deg, #DC3545, #CE0206); color: #FFF;"
+            : "#EFEFEF"};
         border: 0px solid transparent;
         box-shadow: 1px 2px 0px rgba(0, 0, 0, .1);
         border-radius: 3px;
